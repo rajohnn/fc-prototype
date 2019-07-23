ae82e2eaa38438288a80b31a16ebd151441153bd
Richard Johnn <rick.johnn@gmail.com>
2019-07-23 12:02:05 -0400

    Initialize FC2 Vue and Quasar

---
+++ src/layouts/Default.vue
@@ -0,0 +1,143 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-white text-grey-8 q-py-xs">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+          icon="menu"
+        />
+
+        <q-btn flat no-caps no-wrap class="q-ml-xs" v-if="$q.screen.gt.xs">
+          <q-icon name="laptop" color="grey" size="28px" />
+          <q-toolbar-title shrink class="text-weight-bold">
+            Freelance Caddy
+          </q-toolbar-title>
+        </q-btn>
+
+        <q-space />
+
+        <div class="YL__toolbar-input-container row no-wrap">
+
+        </div>
+
+        <q-space />
+
+        <div class="q-gutter-sm row items-center no-wrap">
+          <q-btn round dense flat color="grey-8" icon="format_list_numbered" v-if="$q.screen.gt.sm">
+            <q-tooltip>TODO List</q-tooltip>
+          </q-btn>
+          <q-btn round dense flat color="grey-8" icon="launch" v-if="$q.screen.gt.sm">
+            <q-tooltip>Tasks</q-tooltip>
+          </q-btn>
+          <q-btn round dense flat color="grey-8" icon="contact_phone" v-if="$q.screen.gt.sm">
+            <q-tooltip>Leads/Referral</q-tooltip>
+          </q-btn>
+          <q-btn round dense flat color="grey-8" icon="schedule">
+            <q-tooltip>Scheduler</q-tooltip>
+          </q-btn>
+          <q-btn round dense flat color="grey-8" icon="notifications">
+            <q-badge color="red" text-color="white" floating>
+              2
+            </q-badge>
+            <q-tooltip>Notifications</q-tooltip>
+          </q-btn>
+          <q-btn round flat>
+            <q-avatar size="26px">
+              <img src="https://cdn.pixabay.com/photo/2012/04/26/19/43/profile-42914_960_720.png"/>
+            </q-avatar>
+            <q-tooltip>Account</q-tooltip>
+          </q-btn>
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+      show-if-above
+      content-class="bg-grey-2"
+      :width="240"
+    >
+      <q-scroll-area class="fit">
+        <q-list padding>
+          <q-item v-for="link in links1" :key="link.text" v-ripple clickable>
+            <q-item-section avatar>
+              <q-icon color="grey" :name="link.icon" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>{{ link.text }}</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-separator class="q-my-md" />
+
+          <q-item v-for="link in links2" :key="link.text" v-ripple clickable>
+            <q-item-section avatar>
+              <q-icon color="grey" :name="link.icon" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>{{ link.text }}</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-separator class="q-mt-md q-mb-xs" />
+        </q-list>
+      </q-scroll-area>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MyLayout',
+
+  data () {
+    return {
+      leftDrawerOpen: false,
+      search: '',
+      links1: [
+        { icon: 'home', text: 'Dashboard' },
+        { icon: 'assignment', text: 'Projects' },
+        { icon: 'contacts', text: 'Contacts' }
+      ],
+      links2: [
+        { icon: 'folder_shared', text: 'Clients' },
+        { icon: 'account_balance', text: 'Billing' },
+        { icon: 'find_in_page', text: 'Find Resource' },
+        { icon: 'bar_chart', text: 'Reports' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.YL
+  &__toolbar-input-container
+    min-width 100px
+    width 55%
+
+  &__toolbar-input-btn
+    border-radius 0
+    border-style solid
+    border-width 1px 1px 1px 0
+    border-color rgba(0,0,0,.24)
+    max-width 60px
+    width 100%
+
+  &__drawer-footer-link
+    color inherit
+    text-decoration none
+    font-weight 500
+    font-size .75rem
+
+    &:hover
+      color #000
+</style>
---
+++ src/quasar.js
@@ -0,0 +1,61 @@
+import Vue from 'vue'
+
+import './styles/quasar.styl'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+import {
+  Quasar,
+  QLayout,
+  QHeader,
+  QDrawer,
+  QPageContainer,
+  QPage,
+  QToolbar,
+  QToolbarTitle,
+  QBtn,
+  QIcon,
+  QList,
+  QItem,
+  QItemSection,
+  QItemLabel,
+  QSpace,
+  QInput,
+  Ripple,
+  QSeparator,
+  QTooltip,
+  QScrollArea,
+  QBadge,
+  QAvatar
+} from 'quasar'
+
+Vue.use(Quasar, {
+  config: {},
+  components: {
+    QLayout,
+    QHeader,
+    QDrawer,
+    QPageContainer,
+    QPage,
+    QToolbar,
+    QToolbarTitle,
+    QBtn,
+    QIcon,
+    QList,
+    QItem,
+    QItemSection,
+    QItemLabel,
+    QSpace,
+    QInput,
+    QSeparator,
+    QTooltip,
+    QScrollArea,
+    QBadge,
+    QAvatar
+  },
+  directives: {
+    Ripple
+  },
+  plugins: {
+  }
+})
